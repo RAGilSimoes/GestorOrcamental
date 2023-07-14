aea2ec9b8205df53473c1d3b304387d9b1e3b7e8
Ricardo Andre Simoes <ricardo.andre.simoes@gmail.com>
2023-07-14 19:27:37 +0100

    Arranque do projecto

---
+++ src/GestorOrcamental1.java
@@ -0,0 +1,12 @@
+public class GestorOrcamental1 {
+    
+    public static void main(String[ ] args)
+    {
+        System.out.println ();
+        System.out.println("Bem-vindo ao seu gestor orçamental.");
+        for (String param : args) {
+             System.out.println("Parametro:" + param);
+        }
+        System.out.println ();
+    } 
+}
