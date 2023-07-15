9ed36afdde65da4063679526d51ac5cea70621d3
Ricardo Andre Simoes <ricardo.andre.simoes@gmail.com>
2023-07-15 15:09:15 +0100

    2ª parte do gestor orçamental (calcular despesa, saldo e apresentar)

--- src/GestorOrcamental1.java
+++ src/GestorOrcamental1.java
@@ -4,10 +4,22 @@ public class GestorOrcamental1 {
     {
         System.out.println ();
         System.out.println("Bem-vindo ao seu gestor orçamental.");
+        System.out.println ();
         //System.out.println("===================================");
         //for (String param : args) {
         //     System.out.println("Parametro:" + param);
         //}
-        System.out.println ();
+
+        double compra1 = 19.25;
+        double compra2 = 12.5;
+        double compra3 = 15.3;
+        double orcamento = 80;
+        double despesa_total = compra1 + compra2 + compra3;
+        double saldo_final = orcamento - despesa_total;
+        double saldo_final_percentagem = saldo_final /orcamento*100;
+
+        System.out.println("Hoje gastou " + despesa_total + " euros.");
+        System.out.println("Ficou com " + saldo_final + " euros.");
+        System.out.println("Gastou " + despesa_total/orcamento*100 + "% do seu orçamento e ficou com " + saldo_final_percentagem + "% do seu orçamento.");
     } 
 }
