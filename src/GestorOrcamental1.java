cce1259103062bebb847f480533ae8318942dec4
Ricardo Andre Simoes <ricardo.andre.simoes@gmail.com>
2023-07-18 09:50:10 +0100

    4ª parte do gestor orçamental (introdução de um menu para o utilizador escolher a ação que pretende efetuar; execução da 1º escolha)

--- src/GestorOrcamental1.java
+++ src/GestorOrcamental1.java
@@ -18,30 +18,54 @@ public class GestorOrcamental1 {
         System.out.println("3 - Ver compras efetuadas ");
         System.out.println("4 - Ver a média semanal ");
         System.out.println("5 - Sair ");
+        System.out.println("Introduza aqui a opção:");
         escolha = Le.umInt();
 
-        switch (escolha) {
-            case '1' -> {
-                double compra1, compra2, compra3, orcamento;
+        if (escolha == 1) {
+                double compra1, orcamento;
                 System.out.print("Introduza o valor da 1ª compra: ");
                 compra1 = Le.umDouble();
-                System.out.print("Introduza o valor da 2ª compra: ");
-                compra2 = Le.umDouble();
-                System.out.print("Introduza o valor da 3ª compra: ");
-                compra3 = Le.umDouble();
                 System.out.print("Introduza o valor do orçamento: ");
                 orcamento = Le.umDouble();
-                double despesa_total = compra1 + compra2 + compra3;
-                double saldo_final = orcamento - despesa_total;
-                double saldo_final_percentagem = saldo_final / orcamento * 100;
-                System.out.println("Gastou " + despesa_total + " euros.");
-                System.out.println("Ficou com " + saldo_final + " euros.");
-                System.out.println("Gastou " + despesa_total / orcamento * 100 + "% do seu orçamento e ficou com " + saldo_final_percentagem + "% do seu orçamento.");
-            }
-            case '2' -> System.out.println("Ainda em desenvolvimento");
-            case '3' -> System.out.println("Ainda em desenvolvimento");
-            case '4' -> System.out.println("Ainda em desenvolvimento");
-            case '5' -> System.out.println("Ainda em desenvolvimento");
+                double despesa_total = compra1;
+
+                if (despesa_total > orcamento) {
+                    System.out.println("Não é possível efetuar a compra.");
+                } else {
+                    if (despesa_total == orcamento) {
+                        despesa_total = orcamento;
+                        double gastos_em_percentagem = despesa_total / orcamento * 100;
+                        double saldo_final = orcamento - despesa_total;
+                        double saldo_final_percentagem = saldo_final / orcamento * 100;
+                        System.out.println("Gastou " + despesa_total + " euros.");
+                        System.out.println("Ficou com " + saldo_final + " euros.");
+                        System.out.println("Gastou " + gastos_em_percentagem + "% do seu orçamento e ficou com " + saldo_final_percentagem + "% do seu orçamento.");
+
+                    } else {
+                        double gastos_em_percentagem = despesa_total / orcamento * 100;
+                        double saldo_final = orcamento - despesa_total;
+                        double saldo_final_percentagem = saldo_final / orcamento * 100;
+                        System.out.println("Gastou " + despesa_total + " euros.");
+                        System.out.println("Ficou com " + saldo_final + " euros.");
+                        System.out.println("Gastou " + gastos_em_percentagem + "% do seu orçamento e ficou com " + saldo_final_percentagem + "% do seu orçamento.");
+                    }
+                }
+        }
+
+        if (escolha == 2) {
+            System.out.println("2 - Eliminar a última compra");
+        }
+
+        if (escolha == 3) {
+            System.out.println("3 - Ver compras efetuadas");
+        }
+
+        if (escolha == 4) {
+            System.out.println("4 - Ver a média semanal");
+        }
+
+        if (escolha == 5) {
+            System.out.println("5 - Sair");
         }
     } 
 }
