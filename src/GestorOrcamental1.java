3985dd67d3f6c631ecdf98caeeb147d6861a55d4
Ricardo Andre Simoes <ricardo.andre.simoes@gmail.com>
2023-07-19 21:01:43 +0100

    Otimização do programa

--- src/GestorOrcamental1.java
+++ src/GestorOrcamental1.java
@@ -34,40 +34,7 @@ public class GestorOrcamental1 {
             }
 
             switch (escolha) {
-                case 1:
-                    double compra1, orcamento;
-                    System.out.print("Introduza o valor da 1ª compra: ");
-                    compra1 = Le.umDouble();
-                    System.out.print("Introduza o valor do orçamento: ");
-                    orcamento = Le.umDouble();
-                    double despesa_total = compra1;
-
-                    if (despesa_total < 0 && orcamento < 0) {
-                        System.out.println("Valores inválidos.");
-                    } else {
-
-                        if (despesa_total > orcamento) {
-                            System.out.println("Não é possível efetuar a compra.");
-                        } else {
-                            if (despesa_total == orcamento) {
-                                despesa_total = orcamento;
-                                double gastos_em_percentagem = despesa_total / orcamento * 100;
-                                double saldo_final = orcamento - despesa_total;
-                                double saldo_final_percentagem = saldo_final / orcamento * 100;
-                                System.out.println("Gastou " + despesa_total + " euros.");
-                                System.out.println("Ficou com " + saldo_final + " euros.");
-                                System.out.println("Gastou " + gastos_em_percentagem + "% do seu orçamento e ficou com " + saldo_final_percentagem + "% do seu orçamento.");
-
-                            } else {
-                                double gastos_em_percentagem = despesa_total / orcamento * 100;
-                                double saldo_final = orcamento - despesa_total;
-                                double saldo_final_percentagem = saldo_final / orcamento * 100;
-                                System.out.println("Gastou " + despesa_total + " euros.");
-                                System.out.println("Ficou com " + saldo_final + " euros.");
-                                System.out.println("Gastou " + gastos_em_percentagem + "% do seu orçamento e ficou com " + saldo_final_percentagem + "% do seu orçamento.");
-                            }
-                        }
-                    } break;
+                case 1: fazerCompras();break;
 
                 case 2:
                     System.out.println("2 - Eliminar a última compra ");
@@ -87,5 +54,43 @@ public class GestorOrcamental1 {
 
         } while (escolha != 5);
 
-    } 
+    }
+
+    public static void fazerCompras() {
+        double compra1, orcamento;
+        System.out.print("Introduza o valor da 1ª compra: ");
+        compra1 = Le.umDouble();
+        System.out.print("Introduza o valor do orçamento: ");
+        orcamento = Le.umDouble();
+        double despesa_total = compra1;
+
+        if (despesa_total < 0 && orcamento < 0) {
+            System.out.println("Valores inválidos.");
+        } else {
+
+            if (despesa_total > orcamento) {
+                System.out.println("Não é possível efetuar a compra.");
+            } else {
+                if (despesa_total == orcamento) {
+                    despesa_total = orcamento;
+                    double gastos_em_percentagem = despesa_total / orcamento * 100;
+                    double saldo_final = orcamento - despesa_total;
+                    double saldo_final_percentagem = saldo_final / orcamento * 100;
+                    System.out.println("Gastou " + despesa_total + " euros.");
+                    System.out.println("Ficou com " + saldo_final + " euros.");
+                    System.out.println("Gastou " + gastos_em_percentagem + "% do seu orçamento e ficou com " + saldo_final_percentagem + "% do seu orçamento.");
+
+                } else {
+                    double gastos_em_percentagem = despesa_total / orcamento * 100;
+                    double saldo_final = orcamento - despesa_total;
+                    double saldo_final_percentagem = saldo_final / orcamento * 100;
+                    System.out.println("Gastou " + despesa_total + " euros.");
+                    System.out.println("Ficou com " + saldo_final + " euros.");
+                    System.out.println("Gastou " + gastos_em_percentagem + "% do seu orçamento e ficou com " + saldo_final_percentagem + "% do seu orçamento.");
+                }
+            }
+        }
+    }
 }
+
+
